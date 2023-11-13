18d396c793bb6e3107cb70d871f65f63a2a39964
lalan <lelou_b@naver.com>
2023-11-13 10:57:38 +0900

    Test 2023 11 13

---
+++ cantata/src/main/java/com/packt/cantata/CantataApplication.java
@@ -0,0 +1,14 @@
+package com.packt.cantata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CantataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CantataApplication.class, args);
+		System.out.println("hellooooooo");
+	}
+
+}
