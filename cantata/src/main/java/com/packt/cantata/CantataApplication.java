5ff7a56b126770f15e170eca8f84dde5a78ab3c9
SoJaeho <sojh615@naver.com>
2023-11-14 09:38:02 +0900

    ㅎㅎ

--- cantata/src/main/java/com/packt/cantata/CantataApplication.java
+++ cantata/src/main/java/com/packt/cantata/CantataApplication.java
@@ -10,7 +10,7 @@ public class CantataApplication {
 		SpringApplication.run(CantataApplication.class, args);
 
 		System.out.println("hellooooooo영란aa");
-		System.out.println("재호5");
+		System.out.println("재호6");
 		System.out.println("성재성재");
 		System.out.println("조기조기");
 	}
