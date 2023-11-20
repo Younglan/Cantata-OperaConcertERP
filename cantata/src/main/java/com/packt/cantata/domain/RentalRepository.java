f02cdf676edfa3fc40599caa8596866df1d91745
SoJaeho <145527348+SoJaeho@users.noreply.github.com>
2023-11-20 15:34:35 +0900

    [대관] Rental 리포지토리 수정
    일대다 다대일 연결 추가

--- cantata/src/main/java/com/packt/cantata/domain/RentalRepository.java
+++ cantata/src/main/java/com/packt/cantata/domain/RentalRepository.java
@@ -2,6 +2,6 @@ package com.packt.cantata.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface rentalRepository  extends JpaRepository<rental, Long>{
+public interface RentalRepository  extends JpaRepository<Rental, Long>{
 
 }
