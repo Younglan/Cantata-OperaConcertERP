5515ba83acef1f87cf40388e305776851b378c28
hyungicho <max971230@gmail.com>
2023-12-14 17:36:41 +0900

    plant 컨트롤러와 리포지터리 엔티티 추가및 수정

---
+++ cantata/src/main/java/com/packt/cantata/controller/PlantController.java
@@ -0,0 +1,36 @@
+package com.packt.cantata.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.packt.cantata.domain.Plant;
+import com.packt.cantata.domain.PlantRepository;
+
+@RestController
+//@RequiredArgsConstructor
+@CrossOrigin(origins="http://localhost:3000")
+@RequestMapping("/plants")
+public class PlantController {
+	
+	@Autowired
+	private PlantRepository plantrepository;
+	
+	@GetMapping("/getplant")
+	public Iterable<Plant> getplant(){
+		return plantrepository.findAll();
+	}
+	
+	@RequestMapping(value="/plantapp", method = RequestMethod.POST)
+	public ResponseEntity<Plant> postplant(@RequestBody Plant plant ){
+		Plant pav = plantrepository.save(plant);
+		return ResponseEntity.status(HttpStatus.OK).body(pav);
+	}
+
+}
--- cantata/src/main/java/com/packt/cantata/domain/PlantRepository.java
+++ cantata/src/main/java/com/packt/cantata/domain/PlantRepository.java
@@ -2,6 +2,6 @@ package com.packt.cantata.domain;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface PlantRepository extends CrudRepository<Plant, String>{
+public interface PlantRepository extends CrudRepository<Plant, Long>{
 
 }
