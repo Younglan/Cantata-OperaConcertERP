20f188192e419a5837ca58e2923fb72cc90ed7a2
RabbitGrass <145101277+RabbitGrass@users.noreply.github.com>
2024-01-02 18:15:01 +0900

    Update Reply.java

--- cantata/src/main/java/com/packt/cantata/domain/Reply.java
+++ cantata/src/main/java/com/packt/cantata/domain/Reply.java
@@ -8,6 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -36,6 +37,7 @@ public class Reply {
 	private User id; //회원ID
 	
 	@Column(nullable = false)
+	@Lob
 	private String repSub; //글내용
 	
 	@Column(nullable = false)
