56510f670645f77a68e0de229ac8dd9a9648bd20
RabbitGrass <145101277+RabbitGrass@users.noreply.github.com>
2024-01-03 11:17:57 +0900

    Update AdminPage.js

--- front/src/js/page/AdminPage.js
+++ front/src/js/page/AdminPage.js
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 import "../../css/AdminPage.css";
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import Board from "../component/Board";
+import Board from "../component/Board/Board";
 import PerformanceList from '../component/performance/PerformanceList';
 const SERVER_URL = 'http://localhost:8090';
 // import TicketCheck from '../component/Ticket/TicketCheck';
